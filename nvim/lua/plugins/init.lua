678b515423f47569b9c153e9ff3bca7a941f1431
Mudassir <mughalmudassir966@gmail.com>
2025-08-13 20:13:57 +0500

    changed indenting and added logo

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -67,23 +67,91 @@ require('lazy').setup({
       local luasnip = require("luasnip")
 
       cmp.setup({
-          snippet = {
-              expand = function(args)
-                  luasnip.lsp_expand(args.body)
-              end,
-          },
-          mapping = cmp.mapping.preset.insert({
-              ['<C-Space>'] = cmp.mapping.complete(),                   -- Manually trigger completion
-              ['<Tab>'] = cmp.mapping.select_next_item(),               -- Next item
-              ['<S-Tab>'] = cmp.mapping.select_prev_item(),             -- Prev item
-              ['<CR>'] = cmp.mapping.confirm({ select = true }),        -- Confirm selection
-          }),
-          sources = cmp.config.sources({
-              { name = "nvim_lsp" },
-              { name = "luasnip" },
-              { name = "buffer" },
-              { name = "path" },
-          }),
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-Space>'] = cmp.mapping.complete(),                   -- Manually trigger completion
+          ['<Tab>'] = cmp.mapping.select_next_item(),               -- Next item
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),             -- Prev item
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),        -- Confirm selection
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
+  {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    enabled = true,
+    init = false,
+    opts = function()
+      local dashboard = require("alpha.themes.dashboard")
+      local logo = [[
+    ██╗   ██╗███████╗ ██████╗██╗   ██╗██╗███╗   ███╗
+    ██║   ██║██╔════╝██╔════╝██║   ██║██║████╗ ████║
+    ██║   ██║█████╗  ██║     ██║   ██║██║██╔████╔██║
+    ╚██╗ ██╔╝██╔══╝  ██║     ╚██╗ ██╔╝██║██║╚██╔╝██║
+     ╚████╔╝ ███████╗╚██████╗ ╚████╔╝ ██║██║ ╚═╝ ██║
+      ╚═══╝  ╚══════╝ ╚═════╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+      ]]
+
+      dashboard.section.header.val = vim.split(logo, "\n")
+      -- stylua: ignore
+      dashboard.section.buttons.val = {
+        dashboard.button("f", " " .. " Find file",       "<cmd>Telescope find_files<cr>"),
+        dashboard.button("n", " " .. " New file",        [[<cmd> ene <BAR> startinsert <cr>]]),
+        dashboard.button("g", " " .. " Find text",       [[<cmd>Telescope live_grep<cr>]]),
+        dashboard.button("l", "󰒲 " .. " Lazy",            "<cmd> Lazy <cr>"),
+        dashboard.button("q", " " .. " Quit",            "<cmd> qa <cr>"),
+      }
+      for _, button in ipairs(dashboard.section.buttons.val) do
+        button.opts.hl = "AlphaButtons"
+        button.opts.hl_shortcut = "AlphaShortcut"
+      end
+      dashboard.section.header.opts.hl = "AlphaHeader"
+      dashboard.section.buttons.opts.hl = "AlphaButtons"
+      dashboard.section.footer.opts.hl = "AlphaFooter"
+      dashboard.opts.layout[1].val = 8
+      return dashboard
+    end,
+    config = function(_, dashboard)
+      -- close Lazy and re-open when the dashboard is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          once = true,
+          pattern = "AlphaReady",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      require("alpha").setup(dashboard.opts)
+
+      vim.api.nvim_create_autocmd("User", {
+        once = true,
+        pattern = "LazyVimStarted",
+        callback = function()
+          local stats = require("lazy").stats()
+          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          dashboard.section.footer.val = "⚡ Neovim loaded "
+          .. stats.loaded
+          .. "/"
+          .. stats.count
+          .. " plugins in "
+          .. ms
+          .. "ms"
+          pcall(vim.cmd.AlphaRedraw)
+        end,
       })
     end,
   },
